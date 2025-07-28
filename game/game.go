dd71ede41c996eb8e3fa024a34966c7ca9c56489
omar0ali <bajunaidomar@gmail.com>
2025-07-28 15:24:51 -0400

    Creating GameContext and The Interface Entity

---
+++ game/game.go
@@ -0,0 +1,27 @@
+// Package game
+package game
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/omar0ali/tictactoe-game-cli/core"
+)
+
+type (
+	Entity interface {
+		Draw(gs *GameContext)
+		Update(gs *GameContext)
+		InputEvents(event tcell.Event)
+	}
+	GameContext struct {
+		Window   *core.Window
+		entities []Entity
+	}
+)
+
+func (gs *GameContext) AddEntity(entity Entity) {
+	gs.entities = append(gs.entities, entity)
+}
+
+func (gs *GameContext) GetEntities() []Entity {
+	return gs.entities
+}
