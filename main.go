dc4af8c598d49e43c0f8df801e9e5361e36ee12f
omar0ali <bajunaidomar@gmail.com>
2025-07-28 15:26:13 -0400

    Implementing and integration GameContext and The entity interface
    Current the game shows two boxes when getting close to the middle of the
    box is clickable and will display either x or o

--- main.go
+++ main.go
@@ -3,32 +3,44 @@ package main
 import (
 	"github.com/gdamore/tcell/v2"
 	"github.com/omar0ali/tictactoe-game-cli/core"
+	"github.com/omar0ali/tictactoe-game-cli/entities"
 	"github.com/omar0ali/tictactoe-game-cli/game"
 )
 
 func main() {
 	window := core.CreateWindow("TicTacToe")
-	// objects
 
 	// exit channel waiting to get a an exit signal == 0 from either Events() or Update()
 	exit := make(chan int)
 
-	// testing a single box
+	// test
 	width, height := window.Screen.Size()
-	box := game.CreateBoxHolder(game.Point{X: width / 2, Y: height / 2}, 4)
+	box := entities.CreateBoxHolder(entities.Point{X: width / 2, Y: height / 2}, 4)
+	box.SetContent('X')
+	box1 := entities.CreateBoxHolder(entities.Point{X: (width / 2) + 5, Y: height / 2}, 4)
+	box1.SetContent('O')
 	// for large box, increament by even numbers: min set to 4
+
+	// add objects into the game
+	gameState := game.GameContext{Window: &window}
+	gameState.AddEntity(&box)
+	gameState.AddEntity(&box1)
 
 	window.Events(exit,
 		func(event tcell.Event) {
-			box.InputEvents(event, &window)
+			for _, entity := range gameState.GetEntities() {
+				entity.InputEvents(event)
+			}
 		},
 	)
 
 	window.Update(exit,
 		func(delta float64) {
 			// animation goes here
-			box.Update(&window)
-			box.Draw(&window)
+			for _, entity := range gameState.GetEntities() {
+				entity.Update(&gameState)
+				entity.Draw(&gameState)
+			}
 		},
 	)
 
