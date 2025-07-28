dcbf4d010d98c288a64c8ba3b67f45a39107f325
omar0ali <bajunaidomar@gmail.com>
2025-07-28 15:24:27 -0400

    moving boxholder to entity package using GameContext and Entity Interface
    Entity interface helps to ensure all entities such as box or any game
    entity must hold all the required methods such as Update, Draw,
    InputEvents ...

--- entities/boxholder.go
+++ entities/boxholder.go
@@ -1,9 +1,9 @@
-// Package game
-package game
+// Package entities
+package entities
 
 import (
 	"github.com/gdamore/tcell/v2"
-	"github.com/omar0ali/tictactoe-game-cli/core"
+	"github.com/omar0ali/tictactoe-game-cli/game"
 )
 
 type Point struct {
@@ -30,10 +30,10 @@ func CreateBoxHolder(startPoint Point, scale int) boxHolder {
 	return boxHolder
 }
 
-func (b *boxHolder) InputEvents(event tcell.Event, window *core.Window) {
+func (b *boxHolder) InputEvents(event tcell.Event) {
 	switch ev := event.(type) {
 	case *tcell.EventMouse:
-		if ev.Buttons()&tcell.Button1 != 0 {
+		if ev.Buttons() == tcell.Button1 {
 			mouseX, mouseY := ev.Position()
 			if mouseX >= b.sPoints.X && mouseY >= b.sPoints.Y {
 				if mouseX <= b.ePoints.X && mouseY <= b.ePoints.Y {
@@ -78,31 +78,31 @@ func (b *boxHolder) SetContent(rune rune) {
 	b.content = rune
 }
 
-func (b *boxHolder) Update(window *core.Window) {
+func (b *boxHolder) Update(gs *game.GameContext) {
 	if b.Visible {
 		middleX := (b.GetBoxWidth() / 2) + b.sPoints.X
 		middleY := (b.GetBoxHeight() / 2) + b.sPoints.Y
-		window.SetContent(middleX, middleY, b.content)
+		gs.Window.SetContent(middleX, middleY, b.content)
 	}
 }
 
-func (b *boxHolder) Draw(window *core.Window) {
+func (b *boxHolder) Draw(gs *game.GameContext) {
 	// draw corners
-	window.SetContent(b.GetTopLeftCoords().X, b.GetTopLeftCoords().Y, tcell.RuneULCorner)
-	window.SetContent(b.GetTopRightCoords().X, b.GetTopRightCoords().Y, tcell.RuneURCorner)
-	window.SetContent(b.GetBottomLeftCoords().X, b.GetBottomLeftCoords().Y, tcell.RuneLLCorner)
-	window.SetContent(b.GetBottomRightCoords().X, b.GetBottomRightCoords().Y, tcell.RuneLRCorner)
+	gs.Window.SetContent(b.GetTopLeftCoords().X, b.GetTopLeftCoords().Y, tcell.RuneULCorner)
+	gs.Window.SetContent(b.GetTopRightCoords().X, b.GetTopRightCoords().Y, tcell.RuneURCorner)
+	gs.Window.SetContent(b.GetBottomLeftCoords().X, b.GetBottomLeftCoords().Y, tcell.RuneLLCorner)
+	gs.Window.SetContent(b.GetBottomRightCoords().X, b.GetBottomRightCoords().Y, tcell.RuneLRCorner)
 	// draw lines
 	for i := 1; i < b.GetBoxHeight(); i++ {
-		window.SetContent(b.GetTopLeftCoords().X, b.GetTopLeftCoords().Y+i, tcell.RuneVLine)
+		gs.Window.SetContent(b.GetTopLeftCoords().X, b.GetTopLeftCoords().Y+i, tcell.RuneVLine)
 	}
 	for i := 1; i < b.GetBoxHeight(); i++ {
-		window.SetContent(b.GetTopRightCoords().X, b.GetTopRightCoords().Y+i, tcell.RuneVLine)
+		gs.Window.SetContent(b.GetTopRightCoords().X, b.GetTopRightCoords().Y+i, tcell.RuneVLine)
 	}
 	for i := 1; i < b.GetBoxWidth(); i++ {
-		window.SetContent(b.GetTopLeftCoords().X+i, b.GetTopLeftCoords().Y, tcell.RuneHLine)
+		gs.Window.SetContent(b.GetTopLeftCoords().X+i, b.GetTopLeftCoords().Y, tcell.RuneHLine)
 	}
 	for i := 1; i < b.GetBoxWidth(); i++ {
-		window.SetContent(b.GetBottomLeftCoords().X+i, b.GetBottomLeftCoords().Y, tcell.RuneHLine)
+		gs.Window.SetContent(b.GetBottomLeftCoords().X+i, b.GetBottomLeftCoords().Y, tcell.RuneHLine)
 	}
 }
