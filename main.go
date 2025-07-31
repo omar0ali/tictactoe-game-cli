48c0843e146d4fa0aa50a9d0b036504b41facb00
omar0ali <bajunaidomar@gmail.com>
2025-07-31 13:09:16 -0400

    A little clean up and added minor docs before refactoring
    Will need to clean up the code a little more

--- main.go
+++ main.go
@@ -14,22 +14,20 @@ func main() {
 	// exit channel waiting to get a an exit signal == 0 from either Events() or Update()
 	exit := make(chan int)
 
-	// test
-	// width, height := window.Screen.Size()
-	// // for large box, increament by even numbers: min set to 4
-	// box := entities.CreateBoxHolder(utils.Point{X: width / 2, Y: height / 2}, 4)
-	// box.SetContent('X')
 	gridView := views.InitGridView(9, 1, 4, 3, &window)
-	// add objects into the game
 	gameState := game.GameContext{Window: &window, PlayerTurn: game.P1}
-	// gameState.AddEntity(box)
+
+	boxes := []*entities.BoxHolder{}
 	for i := range gridView.GetItems() {
-		box := gridView.GetItems()[i]
-		boxHolder, ok := box.(*entities.BoxHolder)
+		box := gridView.GetItems()[i]              // get each box
+		boxHolder, ok := box.(*entities.BoxHolder) // try to cast (assertion)
 		if !ok {
 			panic("Type assertion failed: not a *BoxHolder")
 		}
-		gameState.AddEntity(boxHolder)
+		gameState.AddEntity(boxHolder)   // add it as an entity
+		boxes = append(boxes, boxHolder) // this boxes used later | To check for winers
+		boxHolder.SetBoxes(&boxes)       // each box should have a ref of all the boxes. Helps to check
+		// who wins after each turn
 	}
 
 	window.Events(exit,
